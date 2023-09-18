b9a12881833e42ff615e26bb2e11267a32b532e3
Blaine Kennedy <blainekennedy@gmail.com>
2023-09-18 11:22:19 -0500

    feat and tests: add #add_vendor and #vendor_names and thier tests

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -4,9 +4,61 @@ require './lib/market'
 
 RSpec.describe Market do
   before :each do
-    @market = Market.new("South Pearl Street Farmers Market")   
+
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")  
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @vendor1.stock(item1, 35) 
+    @vendor1.stock(item2, 7)    
+    @vendor2.stock(item4, 50) 
+    @vendor2.stock(item3, 25)
+    @vendor3.stock(item1, 65)  
+    @market = Market.new("South Pearl Street Farmers Market")
   end
+
   describe "#initialize" do
-    expect(@market).to be_a Market
+    it 'creates a new market' do
+      expect(@market).to be_a Market
+    end
+
+    it 'creates a new market with a name and vendors list' do
+      expect(@market.name).to eq("South Pearl Street Farmers Market")
+      expect(@market.vendors).to eq([])
+    end
+  end
+
+  describe "#add_vendor" do
+    it 'adds a vendor' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.vendors.length).to eq 3
+      expect(@market.vendors.first).to be_a Vendor  
+    end
+  end
+
+  describe "#vendor_names" do
+    it 'returns vendor names' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
+  
+  describe "#vendors_that_sell" do
+    it 'returns vendors that sell' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.vendors_that_sell(@item1).length).to eq 2
+      expect(@market.vendors_that_sell(@item4)).to eq 1 
+    end
   end
 end
